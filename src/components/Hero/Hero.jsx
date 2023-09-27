0a85f883dd5eb5e6a6915253c844653e179e6708
PhilKubsz <philip.kubisz@gmail.com>
2023-09-27 09:04:28 -0700

    Navbar work / Hero Section start with file updates

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl} from "../.././utils"
+
+export const Hero = () => {
+    return <section className={styles.container}>
+       <div className={styles.content}>
+           <h1 className={styles.title}>Hi, I'm Phil</h1>
+           <p className={styles.description}>I'm a software Engineer / Developer</p>
+           <a href ="mailto:philip.kubisz@gmail.com" className={styles.contactBtn}>Contact me</a>
+           </div>
+           <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg}/>
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+    </section>;
+};
