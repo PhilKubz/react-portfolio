bb7180c48f5eddbc8e808c5727dfb4217d3d7e35
PhilKubsz <philip.kubisz@gmail.com>
2023-09-27 21:29:41 -0700

    Experience Updated - Project Component started / fix on multiple images for Experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,40 +1,47 @@
 import React from "react";
 
+import styles from "./Experience.module.css"
 import skills from "../../data/skills.json";
 import history from "../../data/history";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
     return (
-    <section id="experience">
-      <h2>Experience</h2>
-      <div>
-        <div>{
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>{
             skills.map((skill, id) => {
-                return <div key={id}>
-                    <div>
-                       <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                    </div>
-                    <p>{skill.title}</p>
-                </div>
+                return (
+                 <div className={styles.skill} key={id}>
+                   <div className={styles.skillImageContainer}>
+                      <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                   </div>
+                   <p>{skill.title}</p>
+                 </div>
+                );
             }) 
             }</div>
-        <ul>
-            {
-                history.map((historyItem, id) => {
-                    return (
-                    <li key={id}>
-                        <img 
-                        src={getImageUrl(historyItem.imageSrc)}
-                        alt={`${historyItem.organization} Logo`}
-                        />
-                        <div>
-                            <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                        </div>
-                    </li>
-                    );
-                })
-            }
+        <ul className={styles.history}>
+            {history.map((historyItem, id) => {
+               return (
+                 <li key={id} className={styles.historyItem}>
+                    <img 
+                      src={getImageUrl(historyItem.imageSrc)}
+                      alt={`${historyItem.organization} Logo`}
+                    />
+                    <div className={styles.historyItemDetails}>
+                      <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                      <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                      <ul>
+                        {historyItem.experiences.map((experience, id) => {
+                          return <li key={id}>{experience}</li>;
+                        })}
+                      </ul>
+                    </div>
+                 </li>
+                );
+             })}
         </ul>
       </div>
     </section>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
     return <section className={styles.container}>
        <div className={styles.content}>
            <h1 className={styles.title}>Hi, I'm Phil</h1>
-           <p className={styles.description}>I'm a software Engineer / Developer</p>
+           <p className={styles.description}>I'm a Software Engineer / Developer</p>
            <a href ="mailto:philip.kubisz@gmail.com" className={styles.contactBtn}>Contact me</a>
            </div>
            <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg}/>
